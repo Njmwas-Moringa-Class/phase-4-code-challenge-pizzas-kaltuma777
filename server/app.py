f3527174cd87079646279cfa2ce86a2be7b3a665
Kaltuma <Kaltuma.abdi@student.moringaschool.com>
2024-03-23 19:51:54 +0300

    completed code challenge

--- server/app.py
+++ server/app.py
@@ -1,29 +1,99 @@
-#!/usr/bin/env python3
-
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
 from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-DATABASE = os.environ.get(
-    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
-
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-migrate = Migrate(app, db)
 
 db.init_app(app)
-
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return '<h1>Code challenge</h1>'
 
+@app.route('/restaurants')
+def restaurants():
+    restaurants_list = []
+
+    for restaurant in Restaurant.query.all():
+        restaurant_dict = {
+            "address": restaurant.address,
+            "id": restaurant.id,
+            "name": restaurant.name,
+        }
+        restaurants_list.append(restaurant_dict)
+
+    response = make_response(jsonify(restaurants_list), 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+@app.route('/restaurants/<int:id>', methods=['GET', 'DELETE'])
+def restaurant_by_id(id):
+    restaurant = Restaurant.query.filter_by(id=id).first()
+
+    if restaurant is None:
+        return make_response(jsonify({'error': 'Restaurant not found'}), 404)
+
+    if request.method == 'GET':
+        restaurant_dict = restaurant.to_dict()
+        response = make_response(jsonify(restaurant_dict), 200)
+        return response
+
+    elif request.method == 'DELETE':
+        db.session.delete(restaurant)
+        db.session.commit()
+
+        response_body = {
+            "delete_successful": True,
+            "message": "Restaurant deleted",
+        }
+
+        response = make_response(jsonify(response_body), 204)
+        return response
+
+@app.route('/pizzas')
+def pizzas():
+    pizzas_list = []
+
+    for pizza in Pizza.query.all():
+        pizza_dict = {
+            "id": pizza.id,
+            "ingredients": pizza.ingredients,
+            "name": pizza.name,
+        }
+        pizzas_list.append(pizza_dict)
+
+    response = make_response(jsonify(pizzas_list), 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
+
+@app.route('/restaurant_pizzas', methods=['POST'])
+def restaurant_pizza():
+    if request.method == 'POST':
+        try:
+            new_restaurantpizza = RestaurantPizza(
+                price=request.get_json().get("price"),
+                pizza_id=request.get_json().get("pizza_id"),
+                restaurant_id=request.get_json().get("restaurant_id"),
+            )
+
+            db.session.add(new_restaurantpizza)
+            db.session.commit()
+
+            restaurantpizza_dict = new_restaurantpizza.to_dict()
+
+            response = make_response(jsonify(restaurantpizza_dict), 201)
+            return response
+
+        except ValueError as exception_message:
+            return jsonify(errors=[str(exception_message)]), 400
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,15 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
-from sqlalchemy.orm import validates
-from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import validates, relationship
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
-
+db = SQLAlchemy()
 
 class Restaurant(db.Model, SerializerMixin):
     __tablename__ = 'restaurants'
@@ -18,13 +11,12 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
+    pizzas = relationship('Pizza', secondary='restaurant_pizzas', back_populates='restaurants')
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f'<Restaurant {self.name}>'
-
 
 class Pizza(db.Model, SerializerMixin):
     __tablename__ = 'pizzas'
@@ -33,25 +25,31 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
+    restaurants = relationship('Restaurant', secondary='restaurant_pizzas', back_populates='pizzas')
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f'<Pizza {self.name}, {self.ingredients}>'
-
 
 class RestaurantPizza(db.Model, SerializerMixin):
     __tablename__ = 'restaurant_pizzas'
 
     id = db.Column(db.Integer, primary_key=True)
-    price = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id', ondelete='CASCADE'), nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id', ondelete='CASCADE'), nullable=False)
 
-    # add relationships
+    restaurant = relationship('Restaurant', back_populates='restaurant_pizzas')
+    pizza = relationship('Pizza', back_populates='restaurant_pizzas')
 
-    # add serialization rules
+    @validates('price')
+    def validate_price(self, key, price):
+        if not 1 <= price <= 30:
+            raise ValueError('Price must be between 1 and 30.')
+        return price
 
-    # add validation
+    serialize_rules = ('-restaurant.restaurant_pizzas', '-pizza.restaurant_pizzas',)
 
     def __repr__(self):
         return f'<RestaurantPizza ${self.price}>'
